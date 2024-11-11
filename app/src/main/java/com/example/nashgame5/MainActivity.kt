fbbd7eb5aa493081acce6bc9ae2e40e0d9ce10c0
Deserfd <dima67rud@yandex.ru>
2024-11-12 00:25:19 +0300

    второй коммит

---
+++ app/src/main/java/com/example/nashgame5/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.nashgame5
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val imgHint: ImageButton = findViewById(R.id.image_button_hint)
+        imgHint.setOnClickListener {
+            Toast.makeText(this, "Формат: 2;4 или 2,4.\n Избегайте пробелов ", Toast.LENGTH_LONG).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
